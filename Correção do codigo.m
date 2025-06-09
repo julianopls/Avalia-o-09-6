a202c11a3fc4c7b66612d9188daa369da935468d
julianopls <brolezzijuliano@gmail.com>
2025-06-09 08:46:23 -0300

    Correção do codigo.m

---
+++ Correção do codigo.m
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+typedef struct {
+    char nome[50];
+    int idade;
+    float nota;
+} Aluno;
+
+
+Aluno alunos[3];
+
+void listarAlunos() {
+    for (int i = 0; i < 3; i++) {
+    printf("\nAluno %d\n", i + 1);
+    printf("Nome: %s\n", alunos[i].nome);
+    printf("Idade: %d\n", alunos[i].idade);
+    printf("Nota: %.2f\n", alunos[i].nota);
+    }
+}
+
+int main() {
+    for (int i = 0; i < 3; i++) {
+    printf("\nDigite os dados do aluno %d:\n", i + 1);
+    printf("Nome: ");
+    scanf(" %[^\n]", alunos[i].nome);
+    printf("Idade: ");
+    scanf("%d", &alunos[i].idade);
+    printf("Nota: ");
+    scanf("%f", &alunos[i].nota);
+    }
+
+    listarAlunos();
+
+    return 0;
+}
